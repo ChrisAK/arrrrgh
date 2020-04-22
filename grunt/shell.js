166b023b277f74d76bc99fe07389ef00d6e432fc
ChrisAK <ckennelly8910@gmail.com>
2020-04-22 11:27:53 -0400

    alllllll

--- grunt/shell.js
+++ grunt/shell.js
@@ -39,7 +39,6 @@ module.exports = {
   },
   'deploy-prepare': {
     command: [
-      'git branch -D gh-pages || echo "so not removed"',
       'git checkout --orphan gh-pages',
       'git rm --cached \'*\''
     ].join(' && ')
